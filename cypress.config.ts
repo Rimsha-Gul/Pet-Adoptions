fc848b84c5a4649ddfd6211351ba0d4bddafca30
Rimsha-Gul-TH <rimsha@tetrahex.com>
2023-10-16 19:47:56 +0500

    Remove package vulnerabilities

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-import { defineConfig } from "cypress";
-import { plugins } from "./cypress/e2e/plugins/tasks";
+import { defineConfig } from 'cypress'
+import { plugins } from './cypress/e2e/plugins/tasks'
 
 export default defineConfig({
   numTestsKeptInMemory: 100,
   e2e: {
-    baseUrl: "http://localhost:5173",
-    setupNodeEvents: plugins,
-  },
-});
+    baseUrl: 'http:/localhost:5173',
+    setupNodeEvents: plugins
+  }
+})
